2783b3dee83e63675f6762b114e262875b298048
Suchitra Naik <susanaik@gmail.com>
2018-06-17 16:48:15 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-    raise params.inspect
+  
     if params[:username] && params[:password]
       user = User.new(:username => params[:username], :password => params[:password])
     else
